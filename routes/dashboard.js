80a751d4f7ceed5c3f3021c398b98e625361635d
Lucas Saavedra <lucassaavedra@yahoo.com.ar>
2023-01-10 18:49:48 -0300

    fix en la ruta del /me

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-router.get("/", async (req, res) => {
+router.get("/me", async (req, res) => {
     res.json({
         error : null,
         data : {
